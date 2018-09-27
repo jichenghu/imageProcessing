ad7ac31e5670cffe37f8b268ff63651cd7693c17
jichenghu <jicheng@yahoo.com>
2018-09-27 17:19:15 +0800

    joint merge

--- imageProcessing/cvImage.h
+++ imageProcessing/cvImage.h
@@ -2,14 +2,17 @@
 
 #include <atlimage.h>
 
-//#include <opencv.hpp>
+
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/features2d/features2d.hpp"
 #include "opencv2/xfeatures2d/nonfree.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
 
 #include "opencv2/core/types_c.h"
 #include "opencv2/imgproc/imgproc_c.h"
 #include "opencv2/highgui/highgui_c.h"
+#include "opencv2/calib3d/calib3d_c.h"
 
 #include <iostream>
 #include <map>
